aa38a555b2481e68df4f905210f6c6b01fad60ad
Aaron Flynn <aaron.flynn@digital.landregistry.gov.uk>
2018-09-16 12:35:43 +0100

    Done ex5

--- README.md
+++ README.md
@@ -12,5 +12,5 @@ https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
 Using Ruby 2.4.1 (Current LR Skeleton Version)
 
 Next lesson to do:
-https://learnrubythehardway.org/book/ex5.html
+https://learnrubythehardway.org/book/ex6.html
 
---
+++ exercises/ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'Joe Z Bloggs'
+my_age = 100
+my_height = 70
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Lets talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts 'Thats not too bad really'
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "He's got #{my_teeth} teeth, depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight},"
